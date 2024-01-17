b7934b4afcdf3788beee7e7b18610c910ab52090
kein <zk900826@163.com>
2024-01-17 18:47:10 +0800

    11

--- MeshCreator/Assets/CreatMesh.cs
+++ MeshCreator/Assets/CreatMesh.cs
@@ -9,79 +9,68 @@ public class CreatMesh : MonoBehaviour
 {
     public Material material;
 
-    [Header("Z")]
-    public float length = 1;
-    [Header("X")]
-    public float width = 1;
-    public int fenDuan = 0;
-
-    private int[] triangles;
+    public Vector3 centerPos;
+    public float width,height;
+    public int subdivisionX, subdivisionY;
+    
     private Vector2[] uvs;
     private Vector3[] vertices;
-
-    private int trianglesCount;
     private Mesh mesh;
-
     private MeshFilter meshFilter;
 
-    void OnEnable()
+    private void Awake()
     {
-        GameObject obj = Create(width, length, Vector3.zero);
-        obj.GetComponent<MeshRenderer>().material = material;
+        centerPos = transform.position;
+        Create();
+        gameObject.GetComponent<MeshRenderer>().material = material;
     }
 
-    GameObject Create(float width, float length, Vector3 Point)
+
+    GameObject Create()
     {
         GameObject NewObj = gameObject;
         NewObj.AddComponent<MeshFilter>();
         NewObj.AddComponent<MeshRenderer>();
         meshFilter = NewObj.GetComponent<MeshFilter>();
 
-        int vertices_count = 4 + fenDuan * 2;
-        vertices = new Vector3[vertices_count];
+        vertices = new Vector3[(subdivisionX + 1) * (subdivisionY + 1)];
+        for (int i = 0, y = 0; y <= subdivisionY; y++)
+        {
+            for (int x = 0; x <= subdivisionX; x++, i++)
+            {
+                float xpos = x * (width / subdivisionX);
+                float ypos = y * (height / subdivisionY);
+                vertices[i] = new Vector3( centerPos.x + xpos - width / 2, ypos, 0);
+            }
+        }
 
+        int[] triangles = new int[subdivisionX * subdivisionY * 6];
+        for (int ti = 0, vi = 0, y = 0 ; y < subdivisionY; y++,vi++)
+        {
+            for (int x = 0; x < subdivisionX; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 1] = vi + subdivisionX + 1;
+                triangles[ti + 2] = vi + 1;
 
-        //for (int i = 0; i < vertices_count; i++)
-        //{
-        //    float posx = i > 1 && i
-        //    vertices[i] = new Vector3(Point.x - width / 2, Point.y, Point.z);
-
-        //}
-
-
-        vertices[0] = new Vector3(Point.x + width / 2, Point.y, Point.z);
-        vertices[1] = new Vector3(Point.x - width / 2, Point.y, Point.z );
-        vertices[2] = new Vector3(Point.x + width / 2, Point.y, Point.z + length);
-        vertices[3] = new Vector3(Point.x - width / 2, Point.y, Point.z + length);
-
-
-
-
-
-        //int SplitTriangle = 1 * 2;
-        //trianglesCount = SplitTriangle * 3;
-        triangles = new int[6];
-
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
-        triangles[3] = 2;
-        triangles[4] = 1;
-        triangles[5] = 3;
+                triangles[ti + 3] = vi + 1;
+                triangles[ti + 4] = vi + subdivisionX + 1;
+                triangles[ti + 5] = vi + subdivisionX + 2;
+            }
+        }
 
 
 
         uvs = new Vector2[vertices.Length];
         Vector3[] normals = new Vector3[vertices.Length];
 
-        uvs[0] = new Vector2(1 * width, 1 * length);
-        uvs[1] = new Vector2(1 * width, 0);
-        uvs[2] = new Vector2(0, 1 * length);
-        //uvs[3] = new Vector2(0, 0);
+        uvs[0] = new Vector2(1, 1 );
+        uvs[1] = new Vector2(1, 0);
+        uvs[2] = new Vector2(0, 1);
+        uvs[3] = new Vector2(0, 0);
 
 
         mesh = new Mesh();
-        //mesh.name = "cube";
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
